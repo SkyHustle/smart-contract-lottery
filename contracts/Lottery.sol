2131a4abea3c16d8b26d7dd1eef86219132555f7
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-03 15:08:27 -0700

    Check to make sure the deposit value sent is greater than the entrance fee

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,11 +2,20 @@
 
 pragma solidity ^0.8.7;
 
+error Lottery__NotEnoughETHEntered();
+
 contract Lottery {
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        // storing error codes are more efficient than storing strings
+        if (msg.value < i_entranceFee) {
+            revert Lottery__NotEnoughETHEntered();
+        }
     }
 
     function getEntranceFee() public view returns (uint256) {

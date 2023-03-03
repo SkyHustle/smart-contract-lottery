bfd2258cdef3b0dd0880a481b6f5af3ffb827154
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-03 14:56:06 -0700

    Setup entrance fee

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,4 +2,14 @@
 
 pragma solidity ^0.8.7;
 
-contract Lottery {}
+contract Lottery {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+}

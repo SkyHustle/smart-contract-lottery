c31e32c600ffe2a4481a444fbfba8aaa4246663a
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-04 10:39:42 -0700

    Request random number from Chainlink Node

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -5,18 +5,36 @@ pragma solidity ^0.8.7;
 error Lottery__NotEnoughETHEntered();
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 // inherit from VRFConsumerBaseV2
 contract Lottery is VRFConsumerBaseV2 {
     /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_keyHash;
+    uint64 private immutable i_subId;
+    uint16 private constant MINIMUM_REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
 
     /* Events */
     event LotteryEnter(address indexed player);
+    event RequestLotteryWinner(uint256 indexed requestId);
 
-    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 keyHash,
+        uint64 subId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_keyHash = keyHash;
+        i_subId = subId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterLottery() public payable {
@@ -34,6 +52,14 @@ contract Lottery is VRFConsumerBaseV2 {
         // Request random number
         // do something with it when we get it back
         // 2 tx process
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_keyHash, // gasLane max gas price willing to pay for request in wei
+            i_subId, // subscription for funding requests
+            MINIMUM_REQUEST_CONFIRMATIONS, // how many confirmations Chainlink node should wait before responding
+            i_callbackGasLimit, // how much gas to use for the callback to this function
+            NUM_WORDS
+        );
+        emit RequestLotteryWinner(requestId);
     }
 
     // overide this function we get directly from VRFConsumerBaseV2

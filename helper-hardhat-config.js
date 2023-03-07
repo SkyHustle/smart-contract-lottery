e18e8783fa4779018bff4e13c96b6f062959abec
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-06 20:09:49 -0700

    State working on deployment helper

---
+++ helper-hardhat-config.js
@@ -0,0 +1,13 @@
+const { ethers } = require("harhat")
+
+const networkConfig = {
+    5: {
+        name: "goerli",
+        // subscriptionId: "6926",
+        // gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
+        // keepersUpdateInterval: "30",
+        // raffleEntranceFee: ethers.utils.parseEther("0.001"), // 0.001 ETH
+        // callbackGasLimit: "500000", // 500,000 gas
+        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+    },
+}

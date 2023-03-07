dcd7d1c91842211dd6830424649e2723a25a8a9d
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-07 11:20:25 -0700

    Set the correct chainlink Coordinator based on deployment network

--- deploy/01-deploy-lottery.js
+++ deploy/01-deploy-lottery.js
@@ -1,12 +1,23 @@
 import { network } from "hardhat"
+import { networkConfig, developmentChains } from "../helper-hardhat-config"
 
 export default async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts
+    const chainId = network.config.chainId
+    let vrfCoordinatorV2Address
 
+    if (developmentChains.includes(network.name)) {
+        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+    }
+
+    const args = []
     const lottery = await deploy("Lottery", {
         from: deployer,
-        args: [],
+        args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })

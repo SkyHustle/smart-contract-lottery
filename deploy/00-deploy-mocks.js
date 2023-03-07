6d5a4d31ac8a2324d207195f632b6452edec96a6
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-07 11:09:24 -0700

    Complete vrf mock

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,12 +1,26 @@
 import { developmentChains } from "../helper-hardhat-config"
+import { ethers } from "ethers"
+
+const BASE_FEE = ethers.utils.parseEther("0.25") // 0.25 Link cost per request
+const GAS_PRICE_LINK = 1e9 // 1,000,000,000 // calculated value based on the gas price of the chain. Link per gas
 
 // input vars from the hardhat runtime environment
 export default async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const args = [BASE_FEE, GAS_PRICE_LINK]
 
     if (developmentChains.includes(network.name)) {
         log("Local Network Detected, Deploying Mocks...")
         // deploy a mock vrfCoordinator
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: args,
+        })
+        log("Mocks Deployed!")
+        log("------------------------------------------------")
     }
 }
+
+export const tags = ["all", "mocks"]

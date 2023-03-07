85a2ed2648b321154b3ebd6b507a5d1ee2d10225
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-07 12:01:27 -0700

    Add coordinator subscription id and keyhash

--- deploy/01-deploy-lottery.js
+++ deploy/01-deploy-lottery.js
@@ -1,21 +1,31 @@
-import { network } from "hardhat"
+import { network, ethers } from "hardhat"
 import { networkConfig, developmentChains } from "../helper-hardhat-config"
+
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther(3)
 
 export default async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts
     const chainId = network.config.chainId
-    let vrfCoordinatorV2Address
+    let vrfCoordinatorV2Address, subId
 
     if (developmentChains.includes(network.name)) {
-        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
-        vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = await transactionResponse.wait(1)
+        subId = transactionReceipt.events[0].args.subId
+        // Fund the subcription, only need LINK on real network
+        await vrfCoordinatorV2Mock.fundSubcription(subId, VRF_SUB_FUND_AMOUNT)
     } else {
+        // goerli
         vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subId = networkConfig[chainId]["subId"]
     }
 
     const entranceFee = networkConfig[chainId]["entranceFee"]
-    const args = [vrfCoordinatorV2Address, entranceFee]
+    const keyHash = networkConfig[chainId]["keyHash"]
+    const args = [vrfCoordinatorV2Address, entranceFee, keyHash, subId]
     const lottery = await deploy("Lottery", {
         from: deployer,
         args: args,
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -3,8 +3,8 @@ import { ethers } from "hardhat"
 const networkConfig = {
     5: {
         name: "goerli",
-        // subscriptionId: "6926",
-        // gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
+        subId: "6926",
+        keyHash: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 150 gwei
         // keepersUpdateInterval: "30",
         entranceFee: ethers.utils.parseEther("0.001"), // 0.001 ETH
         // callbackGasLimit: "500000", // 500,000 gas
@@ -13,6 +13,7 @@ const networkConfig = {
     31337: {
         name: "hardhat",
         entranceFee: ethers.utils.parseEther("0.001"), // 0.001 ETH
+        keyHash: "12345", // Doesn't matter, since we're mocking
     },
 }
 
